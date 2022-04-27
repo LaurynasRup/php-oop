f759b941c0ac88b21dc47a40a4622378defbdd44
laurynasrup <rupainis.laurynas@gmail.com>
2022-04-27 10:33:25 +0100

    Set up core controller

--- lrmvc/app/libraries/Core.php
+++ lrmvc/app/libraries/Core.php
@@ -16,26 +16,39 @@ class Core {
 
 
     // Check if possts are in the url
-    if (isset($urlArr['posts'])) {
+    if (isset($urlArr['controller'])) {
 
-      $posts = $urlArr['posts'];
+      $controller = $urlArr['controller'];
       
-      if(file_exists('../app/controllers/'. ucwords($posts) . '.php')) {
+      if (file_exists('../app/controllers/'. ucwords($controller) . '.php')) {
         // if exists - set as current controller
-        $this->currentController = ucwords($posts);
+        $this->currentController = ucwords($controller);
         // unset posts
-        unset($urlArr['posts']);
+        unset($urlArr['controller']);
       }
     }
-
-    // Check if second part of url is set
-    
 
     // Require Controller
     require_once '../app/controllers/' . $this->currentController . '.php';
 
     // Instantiate controller class
     $this->currentController = new $this->currentController;
+
+    // Check if second part of url is set
+    if (isset($urlArr['method'])) {
+      $method = $urlArr['method'];
+
+      if (method_exists($this->currentController, $method)) {
+        $this->currentMethod = $method;
+        // unset method
+        unset($urlArr['method']);
+      }
+    }
+
+    $this->params = $urlArr ? array_values($urlArr) : [];
+
+    // Call a callback with array values
+    call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
   }
 
   public function getUrl(){
